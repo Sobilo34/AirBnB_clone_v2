e11b2b420dac0c38178b012c978b3705c78e433a
Sobilo34 <bilalsolih60@gmail.com>
2024-03-23 07:53:54 -0700

    updated state

--- models/state.py
+++ models/state.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class"""
 from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.city import City
 import models
-import sqlalchemy
-from os import getenv
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = "states"
-    type_of_storage = getenv("HBNB_TYPE_STORAGE")
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    if type_of_storage != 'db':
-        @property
-        def cities(self):
-            """
-            Returns the list of City instances with equal state_id
-            """
-            cities = models.storage.all(City)
-            self.__cities = [
-                city for city in cities.values()
-                if city.state_id == self.id
-            ]
-            return self.__cities
+    @property
+    def cities(self):
+        """
+        Returns the list of City instances with equal state_id
+        """
+        return [city for city in models.storage.all(City).values() if city.state_id == self.id]
+
