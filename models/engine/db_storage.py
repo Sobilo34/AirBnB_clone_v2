62bc5a16ecced0f482f7fb0f27b02c7ef395572c
Sobilo34 <bilalsolih60@gmail.com>
2024-03-23 05:39:34 -0700

    updated db_strg with a test file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,5 @@
-import sqlalchemy
-import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
 from models.amenity import Amenity
 from models.city import City
@@ -7,10 +7,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import Base
-
+import os
 
 class DBStorage:
     __engine = None
@@ -24,8 +21,6 @@ class DBStorage:
                                               os.environ['HBNB_MYSQL_HOST'],
                                               os.environ['HBNB_MYSQL_DB']),
                                       pool_pre_ping=True)
-        if os.getenv('HBNB_ENV') == 'test':
-            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """Query on the current database session"""
@@ -54,4 +49,5 @@ class DBStorage:
     def reload(self):
         """Create all tables in the database and create the current database session"""
         Base.metadata.create_all(self.__engine)
-        self.__session = scoped_session(sessionmaker(bind=self.__engine, expire_on_commit=False))
+        Session = sessionmaker(bind=self.__engine)
+        self.__session = scoped_session(Session)
