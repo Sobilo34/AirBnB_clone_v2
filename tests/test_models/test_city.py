3e122415935466a2555603286eb647abac6da00b
Sobilo34 <bilalsolih60@gmail.com>
2024-03-23 15:00:24 -0700

    reversed

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""This is to test City class"""
+""" """
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 
 
-class Test_City(test_basemodel):
-    """ A Unnitest for city """
+class test_City(test_basemodel):
+    """ """
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -23,20 +23,3 @@ class Test_City(test_basemodel):
         new = self.value()
         self.assertEqual(type(new.name), str)
 
-    def test_city_instance(self):
-        city = City()
-        self.assertIsInstance(city, BaseModel)
-        self.assertIsInstance(city, City)
-
-    def test_city_attributes(self):
-        city = City(name="Test City", state_id="test_state_id")
-        self.assertEqual(city.name, "Test City")
-        self.assertEqual(city.state_id, "test_state_id")
-
-    def test_city_relationship(self):
-        city = City()
-        self.assertTrue(hasattr(city, 'places'))
-
-
-if __name__ == '__main__':
-    unittest.main()
