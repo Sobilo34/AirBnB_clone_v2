1dc037f6f0d096807ab5426f449d4ff5b7ed9115
Sobilo34 <bilalsolih60@gmail.com>
2024-04-04 14:23:47 +0100

    101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,24 +1,19 @@
-# Puppet manifest to set up web servers for deployment of web_static
+# Puppet for setup
 
-# Nginx config file
-$configuration = "server {
+$nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By ${hostname};
     root   /var/www/html;
     index  index.html index.htm;
-
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
-
     location /redirect_me {
-        return 301 https://th3-gr00t.tk;
+        return 301 http://www.github.com/hunterxcobby/;
     }
-
     error_page 404 /404.html;
-
     location /404 {
       root /var/www/html;
       internal;
@@ -28,88 +23,83 @@ $configuration = "server {
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
-} ->
+}
 
-file { '/data':
+-> file { '/data':
   ensure  => 'directory'
-} ->
+}
 
-file { '/data/web_static':
+-> file { '/data/web_static':
   ensure => 'directory'
-} ->
+}
 
-file { '/data/web_static/releases':
+-> file { '/data/web_static/releases':
   ensure => 'directory'
-} ->
+}
 
-file { '/data/web_static/releases/test':
+-> file { '/data/web_static/releases/test':
   ensure => 'directory'
-} ->
+}
 
-file { '/data/web_static/shared':
+-> file { '/data/web_static/shared':
   ensure => 'directory'
-} ->
+}
 
-file { '/data/web_static/releases/test/index.html':
+-> file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "Holberton School Puppet\n"
-} ->
+  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
+}
 
-file { '/data/web_static/current':
+-> file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
-} ->
+}
 
-exec { 'chown -R ubuntu:ubuntu /data/':
+-> exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
 file { '/var/www':
   ensure => 'directory'
-} ->
+}
 
-file { '/var/www/html':
+-> file { '/var/www/html':
   ensure => 'directory'
-} ->
+}
 
-file { '/var/www/html/index.html':
+-> file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "Holberton School Nginx\n"
-} ->
+  content => "This is my first upload  in /var/www/index.html***\n"
+}
 
-file { '/var/www/html/404.html':
+-> file { '/var/www/html/404.html':
   ensure  => 'present',
-  content => "Ceci n'est pas une page\n"
-} ->
+  content => "Ceci n'est pas une page - Error page\n"
+}
 
-file { '/etc/nginx/sites-available/default':
+-> file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
-  content => $configuration
-} ->
+  content => $nginx_conf
+}
 
-exec { 'nginx restart':
+-> exec { 'nginx restart':
   path => '/etc/init.d/'
-}# Puppet manifest to set up web servers for deployment of web_static
+}# Puppet for setup
 
-# Nginx config file
-$configuration = "server {
+$nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By ${hostname};
     root   /var/www/html;
     index  index.html index.htm;
-
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
-
     location /redirect_me {
-        return 301 https://th3-gr00t.tk;
+        return 301 http://www.github.com/hunterxcobby/;
     }
-
     error_page 404 /404.html;
-
     location /404 {
       root /var/www/html;
       internal;
@@ -119,65 +109,65 @@ $configuration = "server {
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
-} ->
+}
 
-file { '/data':
+-> file { '/data':
   ensure  => 'directory'
-} ->
+}
 
-file { '/data/web_static':
+-> file { '/data/web_static':
   ensure => 'directory'
-} ->
+}
 
-file { '/data/web_static/releases':
+-> file { '/data/web_static/releases':
   ensure => 'directory'
-} ->
+}
 
-file { '/data/web_static/releases/test':
+-> file { '/data/web_static/releases/test':
   ensure => 'directory'
-} ->
+}
 
-file { '/data/web_static/shared':
+-> file { '/data/web_static/shared':
   ensure => 'directory'
-} ->
+}
 
-file { '/data/web_static/releases/test/index.html':
+-> file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "Holberton School Puppet\n"
-} ->
+  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
+}
 
-file { '/data/web_static/current':
+-> file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
-} ->
+}
 
-exec { 'chown -R ubuntu:ubuntu /data/':
+-> exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
 file { '/var/www':
   ensure => 'directory'
-} ->
+}
 
-file { '/var/www/html':
+-> file { '/var/www/html':
   ensure => 'directory'
-} ->
+}
 
-file { '/var/www/html/index.html':
+-> file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "Holberton School Nginx\n"
-} ->
+  content => "This is my first upload  in /var/www/index.html***\n"
+}
 
-file { '/var/www/html/404.html':
+-> file { '/var/www/html/404.html':
   ensure  => 'present',
-  content => "Ceci n'est pas une page\n"
-} ->
+  content => "Ceci n'est pas une page - Error page\n"
+}
 
-file { '/etc/nginx/sites-available/default':
+-> file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
-  content => $configuration
-} ->
+  content => $nginx_conf
+}
 
-exec { 'nginx restart':
+-> exec { 'nginx restart':
   path => '/etc/init.d/'
 }
