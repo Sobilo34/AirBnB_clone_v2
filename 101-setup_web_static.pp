5410927c4dd830a707f3f106106d6741be0c300c
Sobilo34 <bilalsolih60@gmail.com>
2024-04-04 13:18:07 +0100

    101-setup_web_static.pp updated

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,111 +1,183 @@
-# Puppet manifest to setup web servers for web_static deployment
+# Puppet manifest to set up web servers for deployment of web_static
 
-# Create the /data/ directory if it doesn't exist
+# Nginx config file
+$configuration = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 https://th3-gr00t.tk;
+    }
+
+    error_page 404 /404.html;
+
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
+
 file { '/data':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
+  ensure  => 'directory'
+} ->
 
-# Create the /data/web_static/ directory if it doesn't exist
 file { '/data/web_static':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
-}
+  ensure => 'directory'
+} ->
 
-# Create the /data/web_static/releases/ directory if it doesn't exist
 file { '/data/web_static/releases':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
-}
+  ensure => 'directory'
+} ->
 
-# Create the /data/web_static/shared/ directory if it doesn't exist
-file { '/data/web_static/shared':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
-}
-
-# Create the /data/web_static/releases/test/ directory if it doesn't exist
 file { '/data/web_static/releases/test':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
-}
+  ensure => 'directory'
+} ->
 
-# Create a fake HTML file /data/web_static/releases/test/index.html
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+
 file { '/data/web_static/releases/test/index.html':
-  ensure  => 'file',
-  owner   => 'root',
-  group   => 'root',
-  content => '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>',
-}
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
 
-# Create a symbolic link /data/web_static/current linked to /data/web_static/releases/test/
 file { '/data/web_static/current':
   ensure => 'link',
-  target => '/data/web_static/releases/test',
-}# Puppet manifest to setup web servers for web_static deployment
+  target => '/data/web_static/releases/test'
+} ->
 
-# Create the /data/ directory if it doesn't exist
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $configuration
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
+}# Puppet manifest to set up web servers for deployment of web_static
+
+# Nginx config file
+$configuration = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 https://th3-gr00t.tk;
+    }
+
+    error_page 404 /404.html;
+
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
+
 file { '/data':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
+  ensure  => 'directory'
+} ->
 
-# Create the /data/web_static/ directory if it doesn't exist
 file { '/data/web_static':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
-}
+  ensure => 'directory'
+} ->
 
-# Create the /data/web_static/releases/ directory if it doesn't exist
 file { '/data/web_static/releases':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
-}
+  ensure => 'directory'
+} ->
 
-# Create the /data/web_static/shared/ directory if it doesn't exist
-file { '/data/web_static/shared':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
-}
-
-# Create the /data/web_static/releases/test/ directory if it doesn't exist
 file { '/data/web_static/releases/test':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
-}
+  ensure => 'directory'
+} ->
 
-# Create a fake HTML file /data/web_static/releases/test/index.html
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+
 file { '/data/web_static/releases/test/index.html':
-  ensure  => 'file',
-  owner   => 'root',
-  group   => 'root',
-  content => '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>',
-}
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
 
-# Create a symbolic link /data/web_static/current linked to /data/web_static/releases/test/
 file { '/data/web_static/current':
   ensure => 'link',
-  target => '/data/web_static/releases/test',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $configuration
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
