1608687f0baf9f018c04a000cc7535794ed6620c
Sobilo34 <bilalsolih60@gmail.com>
2024-04-04 14:01:13 +0100

    Resversed back to initial

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
-# a Bash script that sets up your web servers for the deployment of web_static
+# A bash script that sets up your web servers for the deployment of web_static
 
 # Install Nginx if not already installed
-sudo apt-get update
-sudo apt-get -y install nginx
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install nginx -y
+fi
 
-# Create necessary folders
+# Create necessary directories
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 # Create fake HTML file
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+sudo touch /data/web_static/releases/test/index.html
+echo "<html><head><title>Test Page</title></head><body>This is a test page.</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
 # Create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-# Set ownership
+# Set ownership of /data/ directory recursively
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-sudo sed -i 's|^.*server_name.*$|&\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n|' /etc/nginx/sites-available/default
-
+sudo sed -i '/^\s*location \/hbnb_static {/a \\talias /data/web_static/current/;' /etc/nginx/sites-available/default
 
 # Restart Nginx
 sudo service nginx restart
