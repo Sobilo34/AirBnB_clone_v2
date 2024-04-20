f4cbcc0edeee259bd58187674dd095b3f77ba186
Sobilo34 <bilalsolih60@gmail.com>
2024-04-20 15:16:25 +0100

    render template for routing

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Script to start a Flask web application.
+"""
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def say_hello():
+    """Route to display 'Hello HBNB!'."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def say_hbnb():
+    """Route to display 'HBNB'."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """Route to display 'C' followed by the value of the text variable."""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+def display_py_text(text):
+    """Route to display 'C' followed by the value of the text variable."""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_n_number(n):
+    """Route to display “n is a number” only if n is an integer"""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_n_number(n):
+    """Route to display “n is a number” only if n is an integer"""
+    return render_template('5-index.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
