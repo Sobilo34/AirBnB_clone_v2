6ea345f89f26159888aa45cab0258e631b568dbe
Sobilo34 <bilalsolih60@gmail.com>
2024-04-20 15:51:33 +0100

    rewrite the file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -10,34 +10,34 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Function called through the / route."""
+def say_hello():
+    """Route to display 'Hello HBNB!'."""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Function called through the /hbnb route."""
+def say_hbnb():
+    """Route to display 'HBNB'."""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """Function called through the /c/<text> route."""
-    return 'C ' + text.replace('_', ' ')
+def display_c_text(text):
+    """Route to display 'C' followed by the value of the text variable."""
+    return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text):
-    """Function called through the /python/<text> route."""
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+def display_py_text(text):
+    """Route to display 'C' followed by the value of the text variable."""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    """Function that displays "n is a number" if n is indeed an integer."""
-    return '{:d} is a number'.format(n)
+def display_n_number(n):
+    """Route to display “n is a number” only if n is an integer"""
+    return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
@@ -47,4 +47,4 @@ def number_template(n):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
