f90e2bc106ea2fe7ac440f94077290c3865cdf24
Sobilo34 <bilalsolih60@gmail.com>
2024-04-20 14:52:50 +0100

    added the expected parameter

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,10 +22,10 @@ def say_hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def display_c_text():
+def display_c_text(text):
     """Route to display 'C' followed by the value of the text variable."""
-    return "C " + text.replace('_', ' ')
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
