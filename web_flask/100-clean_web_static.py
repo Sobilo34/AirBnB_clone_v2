f556fcf3c3e48acb9839fd0cb0c04e6b5a05981a
Sobilo34 <bilalsolih60@gmail.com>
2024-04-23 23:49:57 +0100

    update all

---
+++ web_flask/100-clean_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Deletes out-of-date archives using the function do_clean.
+"""
+
+import os
+from fabric.api import *
+
+# Servers' IP addresses
+env.hosts = ['54.90.14.221', '204.236.240.155']
+
+
+def do_clean(number=0):
+    """
+    Deletes out-of-date archives.
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    # Local cleaning
+    zips = sorted(os.listdir("versions"))
+    [zips.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in zips]
+
+    # Remote cleaning
+    with cd("/data/web_static/releases"):
+        zips = run("ls -tr").split()
+        zips = [a for a in zips if "web_static_" in a]
+        [zips.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in zips]
